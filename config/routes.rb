546a25db8add6123c0d627d00b75ca6307726d71
lynchlcl71 <lynchlcl@aol.com>
2017-05-05 17:14:13 -0400

    Built paages via controller generator

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'pages/home'
+    @posts = Blog.all
+  get 'pages/about'
+
+  get 'pages/contact'
+
   resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
